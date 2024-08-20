3b0b05c3b43d43a39996b6f169f29c3be63812bd
Victor Abrokwah <kingportipher@outlook.com>
2024-08-20 22:14:55 +0000

    Task 0 fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
+
 """
-This module provides a function 
+module provides a function to create an asyncio Task
 """
+import asyncio
+import random
+from typing import Generator
 
-from asyncio import Task, create_task
-from 0_basic_async_syntax import wait_random
-
-
-def task_wait_random(max_delay: int) -> Task:
+async def async_generator() -> Generator[ float, None, None]:
     """
-    Create and return an asyncio Task for wait_random
+    Create and return an asyncio Task for wait_random with the given max_delay.
     """
-    return create_task(wait_random(max_delay))
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random( * 10

04ecbcee0830b31c8610a462b153cde7ff05c84d
Victor Abrokwah <kingportipher@outlook.com>
2024-08-20 22:41:54 +0000

    Task 1 done

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,13 @@
 This module defines an asynchronous coroutine `async_comprehension`.
 """
 
+from typing import List
 from . import async_generator  # Import async_generator from the previous file
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
   """
   This coroutine collects 10 random numbers.
   """
-  return [await value async for value in async_generator()]
+  return [value async for value in async_generator()]
+
